efffc95990660a76ca184bfac6cc8ef3c810c3c9
zoo0406 <zoo0406.jyl@gmail.com>
2025-04-04 16:23:14 +0900

    4월 4일 11단계 중간 저장

---
+++ src/level11/b24262/Main.java
@@ -0,0 +1,11 @@
+package level11.b24262;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        System.out.print("1\n0");
+    }
+}
